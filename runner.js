d94a7fc44f12d4cf4e3438dc86ffbe656ce70189
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-24 22:40:03 -0700

    require fs and add argument to method

--- runner.js
+++ runner.js
@@ -1,13 +1,15 @@
+const fs = require('fs');
+
 class Runner {
     constructor() {
         // store a reference to every file we discover in the directory
         this.files = [];
     }
 
-    collectFiles(){
+    async collectFiles(targetPath){
         // iterate through all the different folders of a project
         // and add it to the this.files array
-        
+
     }
 }
 
