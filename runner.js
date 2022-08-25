3181b7459ccaf0c3111c777637cb6e62a2c02a54
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-24 23:47:43 -0700

    print out only the errors message

--- runner.js
+++ runner.js
@@ -32,7 +32,7 @@ class Runner {
                     console.log(`OK - ${description}`);
                 } catch (err) {
                     console.log(`X - ${description}`);
-                    console.log(err);
+                    console.log(err.message);
                 }
                 
             };
