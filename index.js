72d0a507fd454b0e05f2ae3ecd071a741b2cf131
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-24 22:46:11 -0700

    require runner and make helper function for executing collect files method, then test

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 // After configuring package.json run npm link in terminal
+const Runner = require('./runner');
 
-console.log('Running test!');
+const runner = new Runner();
+
+// helper function for running async/await
+const run = async () => {
+    const results = await runner.collectFiles(process.cwd()); // current working directory
+
+    console.log(results);
+};
+
+run();
