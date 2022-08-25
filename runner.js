d72b2735b5ba908ea327ad3a85b0aad1aa71b218
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-24 22:37:28 -0700

    prepare constructor and method with notes for how to implement

---
+++ runner.js
@@ -0,0 +1,14 @@
+class Runner {
+    constructor() {
+        // store a reference to every file we discover in the directory
+        this.files = [];
+    }
+
+    collectFiles(){
+        // iterate through all the different folders of a project
+        // and add it to the this.files array
+        
+    }
+}
+
+module.exports = Runner;
