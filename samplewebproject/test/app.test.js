d3263684af4cf0c9b67796198599a4723423f17b
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-25 22:28:30 -0700

    finalize test assertion

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -21,5 +21,5 @@ it('shows a success message with a valid email', async () => {
 
     const h1 = dom.window.document.querySelector('h1');
 
-    console.log('Contents of h1', h1.innerHTML);
+    assert.strictEqual(h1.innerHTML, 'Looks good!');
 });
