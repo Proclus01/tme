f18df9dc7c95dc19f82dab5848dad3bd57d022ef
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-25 21:46:10 -0700

    create new file for rendering html elements using jsdom

---
+++ render.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const render = (filename) => {
+    const filePath = path.join(process.cwd(), filename);
+};
