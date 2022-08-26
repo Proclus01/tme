c8a4cd8aacba7caed149debb33e6a0b86dee083c
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-25 21:07:30 -0700

    create new test project directory for testing apps in the browser using jsdom

---
+++ samplewebproject/index.js
@@ -0,0 +1,11 @@
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const { value } = document.querySelector('input');
+
+    if (value.includes('@')) {
+        // must be valid
+    } else {
+        // must be invalid
+    }
+});
