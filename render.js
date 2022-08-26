444bb6ffbbcad1e4d504c6f261efb05d91c8b7a0
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-25 21:49:23 -0700

    create helper function to render html code using jsdom and export function

--- render.js
+++ render.js
@@ -2,6 +2,15 @@ const path = require('path');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const render = (filename) => {
+const render = async (filename) => {
     const filePath = path.join(process.cwd(), filename);
+
+    const dom = await JSDOM.fromFile(filePath, {
+        runScripts: 'dangerously', // only to execute code we've only authored
+        resources: 'usable'
+    });
+
+    return dom;
 };
+
+module.exports = render;
